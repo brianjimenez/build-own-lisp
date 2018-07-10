5c57995c033ba6a51a4df75c24132dcaaa773f53
Brian Jimenez <bjimenez@bsc.es>
2018-07-10 10:22:19 +0200

    Use min and max tags

--- lispy.c
+++ lispy.c
@@ -36,8 +36,8 @@ long eval_op(long x, char* op, long y) {
   if (strcmp(op, "/") == 0) { return x / y; }
   if (strcmp(op, "%") == 0) { return x % y; }
   if (strcmp(op, "^") == 0) { return (int) pow((double) x,y); }
-  if (strcmp(op, "m") == 0) { return ( (x <= y) ? x : y ); }
-  if (strcmp(op, "M") == 0) { return ( (x >= y) ? x : y ); }
+  if (strcmp(op, "min") == 0) { return ( (x <= y) ? x : y ); }
+  if (strcmp(op, "max") == 0) { return ( (x >= y) ? x : y ); }
   return 0;
 }
 
@@ -77,7 +77,8 @@ int main(int argc, char** argv) {
   mpca_lang(MPCA_LANG_DEFAULT,
   "                                                                     \
     number   : /-?[0-9]+/ ;                                             \
-    operator : '+' | '-' | '*' | '/' | '%' | '^' | 'm' | 'M' ;          \
+    operator : '+' | '-' | '*' | '/' | '%' | '^' |                      \
+                \"min\" | \"max\" ;                                     \
     expr     : <number> | '(' <operator> <expr>+ ')' ;                  \
     lispy    : /^/ <operator> <expr>+ /$/ ;                             \
   ",
